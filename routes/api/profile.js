3d4022a69c3cbfa48cbc9fc3672f3e584a1db6a6
davidka7 <david.perekopskiy@go.shoreline.edu>
2021-01-09 21:34:37 -0800

    profile donw

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
-const keys = require("../../config/keys");
+
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
 
 // Load Profile Model
 const Profile = require("../../models/Profile");
@@ -25,6 +27,7 @@ router.get(
     Profile.findOne({
       user: req.user.id,
     })
+      .populate("user", ["name", "avatar"])
       .then((profile) => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -43,14 +46,22 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = relocationq.body.comapny;
+    if (req.body.company) profileFields.company = req.body.comapny;
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.bbioody.bio;
+    if (req.body.bio) profileFields.bio = req.body.bio;
     if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername)
       profileFields.githubusername = req.body.githubusername;
