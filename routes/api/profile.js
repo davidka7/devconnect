18621cb852d5081e03e44cc66e08da18495cc2c2
davidka7 <david.perekopskiy@go.shoreline.edu>
2021-01-10 17:54:48 -0800

    profile edit route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,8 +39,42 @@ router.get(
   }
 );
 
+// @route Get api/profile/handle/:handle
+// @desc  Get profile by handle
+// @acces Public
+
+router.get("/handle/:handle", (req, res) => {
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
+// @route Get api/profile/user/:user_id
+// @desc  Get profile by user ID
+// @acces Public
+
+router.get("/user/:user_id", (req, res) => {
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
 // @route Post api/profile
-// @desc  Create users profile
+// @desc  Create or edit user profile
 // @acces Private
 router.post(
   "/",
